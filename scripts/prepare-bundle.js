e147f6f615dba25cc0ca1c1f112f46b947ebc26b
Markus <markus.goeckeritz@sixa.ch>
2021-09-21 00:07:42 +0200

    polished code

--- scripts/prepare-bundle.js
+++ scripts/prepare-bundle.js
@@ -1,9 +1,16 @@
+/**
+ * Internal dependencies.
+ */
 const { UNUSED_COMPOSER_DEPENDENCIES, getProjectPath, runCommand } = require( '../utils' );
 
 /**
+ * Return true if the current package requires any composer production dependencies.
+ * Notice that we are filtering the production dependencies from the `composer.json`
+ * with a whitelist of dependencies that are added in `require` but which are not
+ * needed in production (e.g. composer plugins).
  *
- * @param  whitelist
- * @return {boolean}
+ * @param     {array}      whitelist    List of package names to skip.
+ * @return    {boolean}                 True if package contains production dependencies.
  */
 const hasComposerProductionDependencies = ( whitelist = UNUSED_COMPOSER_DEPENDENCIES ) => {
 	const composerJson = require( getProjectPath( 'composer.json' ) );
@@ -17,6 +24,7 @@ const hasComposerProductionDependencies = ( whitelist = UNUSED_COMPOSER_DEPENDEN
 	const keys = Object.keys( composerJson.require );
 	for ( let i = 0; i < keys.length; i++ ) {
 		const key = keys[ i ];
+		// Bail early and return true if the current key is not a whitelisted dependency.
 		if ( ! whitelist.includes( key ) ) {
 			return true;
 		}
@@ -25,6 +33,15 @@ const hasComposerProductionDependencies = ( whitelist = UNUSED_COMPOSER_DEPENDEN
 	return false;
 };
 
+/**
+ * Perform the `prepare` operation.
+ * The `prepare` operation:
+ *   i)   Installs NPM dependencies
+ *   ii)  Builds bundle assets
+ *   iii) Installs composer production dependencies
+ *
+ * Notice that this function is called at the bottom of this file.
+ */
 const prepareBundle = () => {
 	console.log( 'Installing NPM dependencies...' );
 	runCommand( 'npm', [ 'install' ] );
@@ -34,11 +51,14 @@ const prepareBundle = () => {
 	runCommand( 'npm', [ 'run', 'build' ] );
 	console.log( 'Built bundle assets.' );
 
+	// Only install composer dependencies if there are production dependencies.
 	if ( hasComposerProductionDependencies() ) {
 		console.log( 'Installing composer dependencies...' );
+		// Make sure to optimize the autoloader.
 		runCommand( 'composer', [ 'install', '--no-dev', '--optimize-autoloader' ] );
 		console.log( 'Installed composer dependencies.' );
 	}
 };
 
+// This file is invoked with `node` and must thus execute the script operation.
 prepareBundle();
