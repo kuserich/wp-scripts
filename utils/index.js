b76813fe53d678620f7d8dcb45fa98b41bb0596f
Markus <markus.goeckeritz@sixa.ch>
2021-09-21 00:03:04 +0200

    polished code

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,16 @@
+/**
+ * Internal dependencies.
+ */
 const { UNUSED_COMPOSER_DEPENDENCIES, BUNDLE_IGNORE, GIT_IGNORE, NPM_IGNORE } = require( './constants' );
-
 const { existsInProject, getProjectPath } = require( './file' );
-
 const { runCommand } = require( './package' );
 const { runScript } = require( './cli' );
 
+/**
+ * This file simply imports and re-exports every export from every
+ * utility module in this package. This creates a single entrypoint
+ * for all scripts to import utility functions from.
+ */
 module.exports = {
 	UNUSED_COMPOSER_DEPENDENCIES,
 	BUNDLE_IGNORE,
