7b2021f8e6257b3f5b720dc90239c2e08fca2292
Markus <markus.goeckeritz@sixa.ch>
2021-09-20 23:58:44 +0200

    polished code

--- utils/cli.js
+++ utils/cli.js
@@ -1,18 +1,42 @@
+/**
+ * Internal dependencies.
+ */
 const { runCommand } = require( './package' );
-const { existsInProject, getScriptPath, pathExists } = require( './file' );
+const { getScriptPath, pathExists } = require( './file' );
 
+/**
+ * Run the script with the given name and pass the given arguments.
+ *
+ * @function
+ * @since       1.0.0
+ * @param       {string}    name    Name of the script.
+ * @param       {array}     args    Arguments for the script.
+ * @example
+ *
+ * runScript( 'bundle' );
+ */
 const runScript = ( name, args = [] ) => {
+	// Bail early and print an error if the script name is not provided.
 	if ( ! name ) {
 		console.error( `Script name must be provided.` );
 		process.exit( 1 );
 	}
 
+	// Bail early and print an error if the script doesn't exist.
 	if ( ! pathExists( getScriptPath( name ) ) ) {
 		console.error( `Missing script: "${ name }"` );
 		process.exit( 1 );
 	}
 
-	runCommand( 'node', [ getScriptPath( name ), ...args ], { stdio: [ process.stdin, process.stdout, process.stderr ] } );
+	// Run the script.
+	// Notice that we are passing an `stdio` option to the child process.
+	// This allows us to retrieve and print stdout and stderr logs to the
+	// console of the main process.
+	runCommand(
+		'node',
+		[ getScriptPath( name ), ...args ],
+		{ stdio: [ process.stdin, process.stdout, process.stderr ] }
+	);
 };
 
 module.exports = {
