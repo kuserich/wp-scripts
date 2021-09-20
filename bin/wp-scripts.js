3f559b832e7049bc14391ab4c8e7653ecd97e02d
Markus <markus.goeckeritz@sixa.ch>
2021-09-21 00:14:53 +0200

    polished code

--- bin/wp-scripts.js
+++ bin/wp-scripts.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-// global process
+/**
+ * Internal dependencies.
+ */
 const { runScript } = require( '../utils' );
 
+
+// Call the script passed in `sixa-wp-scripts` with the given arguments.
 const args = process.argv.slice( 2 );
 runScript( args[ 0 ], args.slice( 2 ) );
